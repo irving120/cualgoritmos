af8f7c849942e8ac08fe948793f29d505c2321a6
irving120 <111026263+irving120@users.noreply.github.com>
2022-08-12 19:10:36 -0500

    Add files via upload

---
+++ arreglos.js
@@ -0,0 +1,22 @@
+var x = {"Nombre" : "Irving",
+        "Edad" : "15",
+    "Gustos musicales":{
+        "Trap" : "H.I.E.L.O.",
+        "Electronica" : "losing it",
+    },
+    "Albues favoritos" : {
+        "Trap" : "Atrevido",
+        "Electronica" : "Labrinth",
+    },
+    "Gustos de peliculas" : {
+        "Accion" : "Maze runner",
+        "Terror" : "La monja",
+        "Comedia" : "Que paso ayer?",
+    },
+    "Animales favoritos" : {
+        "Perros" : "Golden",
+        "Peces" : "betta",
+    },
+};
+
+console.log(x);
