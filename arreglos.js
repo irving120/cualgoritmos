241ba42924dc762efab209b1975b2ec3a48f1a1c
irving120 <111026263+irving120@users.noreply.github.com>
2022-08-12 19:10:06 -0500

    Delete arreglos.js

--- arreglos.js
+++
@@ -1,8 +0,0 @@
-var x = {"Nombre" : "Irving",
-        "Edad" : "15",
-        "Gustos Musicales" : "Electronica" + " " + "Trap",
-        "Albumes favoritos" : "Labrinth" + " " + "Atrevido",
-        "Gustos de peliculas" : "Accion" + " " + "Terror" + " " + "Comedia",
-        "Animales favoritos" : "Perros" + " " + "Peces"}
-
-console.log(x);
